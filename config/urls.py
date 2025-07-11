415d77710d6e9a0a803901b099fe7d1abcca48ce
hydra <navidsoleymani@ymail.com>
2025-07-11 13:44:48 -0600

    add swagger... .

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.i18n import i18n_patterns
-from django.utils.translation import gettext_lazy as _
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
+from django.utils.translation import gettext_lazy as _
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
 from config.settings import (
     STATIC_URL,
     STATIC_ROOT,
@@ -11,8 +14,23 @@ from config.settings import (
     MEDIA_ROOT,
 )
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ChronosTasker APIs",
+        default_version='v1',
+        description="Designed for asynchronous and periodic task execution... .",
+        contact=openapi.Contact(email="navidsoleymani@ymail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = (
         [
+            path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+            path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+            path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
             path('api/v1/', include('config.interfaces.v1')),
         ]
         + i18n_patterns(path('admin/', admin.site.urls), )
