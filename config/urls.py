eed83324806c28ded8ce6ed8198f0ba2c9c15213
hydra <navidsoleymani@ymail.com>
2025-07-11 13:47:51 -0600

    fixed :WARNINGS: ?: (2_0.W001) Your URL pattern 'swagger(?P<format>\.json|\.yaml)$' [name='schema-json'] has a route that contains '(?P<', begins with a '^', or ends with a '$'. This was likely an oversight when migrating to django.urls.path().

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.utils.translation import gettext_lazy as _
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -28,7 +28,7 @@ schema_view = get_schema_view(
 
 urlpatterns = (
         [
-            path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+            re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
             path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
             path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
             path('api/v1/', include('config.interfaces.v1')),
