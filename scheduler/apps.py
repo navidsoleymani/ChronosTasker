32de55c8d611d4b94bbeac778fe6c489fe2b57c6
hydra <navidsoleymani@ymail.com>
2025-07-12 00:59:09 -0600

    update for gettext_lazy ....

--- scheduler/apps.py
+++ scheduler/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class SchedulerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scheduler'
+    verbose_name = _('Scheduler')
